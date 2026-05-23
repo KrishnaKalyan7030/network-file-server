b6bde0dbed744b016efe4cde665d697ae33765b3
Krishna_Kalyan <krishnakalyan833@gmail.com>
2026-05-23 21:17:23 +0530

    single chunk of data from file is transferred.

--- app/sockets/client.py
+++ app/sockets/client.py
@@ -7,24 +7,26 @@ try:
     client_socket.connect(('localhost', 9999))
 
     print("Connected to server")
+    file=open('../files/sample.txt','rb')  #rb=> raw bytes
+    chunk=file.read(1024)
+    print(chunk)
+    # while True:
 
-    while True:
+    #     msg = input("Enter message: ")
 
-        msg = input("Enter message: ")
+    #     client_socket.send(msg.encode())
 
-        client_socket.send(msg.encode())
+    #     if msg.lower() == 'exit':
+    #         print("Disconnected from server")
+    #         break
 
-        if msg.lower() == 'exit':
-            print("Disconnected from server")
-            break
+    #     response = client_socket.recv(1024).decode()
 
-        response = client_socket.recv(1024).decode()
+    #     if not response:
+    #         print("Server disconnected")
+    #         break
 
-        if not response:
-            print("Server disconnected")
-            break
-
-        print(response)
+    #     print(response)
 
 except Exception as e:
 
